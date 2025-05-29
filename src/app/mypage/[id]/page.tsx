1d585f84a56a586ee611546c9cdf9a0abae87c71
Jang Eunhye <hh961130@naver.com>
2025-05-29 19:15:04 +0900

    rename: 예시코드 test 페이지로 이동

--- src/app/mypage/[id]/page.tsx
+++ src/app/mypage/[id]/page.tsx
@@ -1,40 +1,7 @@
-import PropertyCard from "@/components/common/PropertyCard";
-
 export default function MyPage({ params }: { params: { id: string } }) {
   return (
-    <div className="flex w-[395px] justify-center bg-blue-500">
-    <div className="w-[375px] min-h-screen bg-black">
-      <h1 className="mb-8 text-2xl font-bold">마이페이지입니다</h1>
-      <div className="w-[375px] h-[116px]">
-        <PropertyCard
-          itemId={1}
-          itemNumber={1}
-          imageUrl="/imgs/-.png"
-          transactionType="전세"
-          price="5억 3,000"
-          address="방배마에스트로{주상복합}"
-          detailAddress="101동 703호"
-          buildingType="아파트"
-          area="34.5㎡"
-          tags={["풀옵션", "xx역 도보 n분", "대학교 인접"]}
-          liked={true}
-        />
-        <PropertyCard
-          itemId={2}
-          itemNumber={1}
-          imageUrl="/imgs/-.png"
-          transactionType="전세"
-          price="5억 3,000"
-          address="방배마에스트로{주상복합}"
-          detailAddress="101동 703호"
-          buildingType="아파트"
-          area="34.5㎡"
-          tags={["풀옵션", "xx역 도보 n분", "대학교 인접"]}
-          liked={true}
-          small={true}
-        />
-      </div>
-      </div>
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-bold">마이페이지입니다</h1>
     </div>
   );
 }
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,22 +1,56 @@
+import PropertyCard from "@/components/common/PropertyCard";
+
 export default function Test() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center gap-1 p-8">
-      <p className="text-largeTitle">Large Title 텍스트</p>
-      <p className="text-title1">Title 1 텍스트</p>
-      <p className="text-title2">Title 2 텍스트</p>
-      <p className="text-title3">Title 3 텍스트</p>
-      <p className="text-title4">Title 4 텍스트</p>
-      <p className="text-subtitle1">Subtitle 1 텍스트</p>
-      <p className="text-subtitle2">Subtitle 2 텍스트</p>
-      <p className="text-subtitle3">Subtitle 3 텍스트</p>
-      <p className="text-subtitle4">Subtitle 4 텍스트</p>
-      <p className="text-body1">Body 1 텍스트</p>
-      <p className="text-body2">Body 2 텍스트</p>
-      <p className="text-body3">Body 3 텍스트</p>
-      <p className="text-caption1">Caption 1 텍스트</p>
-      <p className="text-caption2">Caption 2 텍스트</p>
-      <p className="text-caption3">Caption 3 텍스트</p>
-      <p className="text-footnote">Footnote 텍스트</p>
-    </div>
+    <>
+      <div>
+        <PropertyCard
+          itemId={1}
+          itemNumber={1}
+          imageUrl="/imgs/-.png"
+          transactionType="전세"
+          price="5억 3,000"
+          address="방배마에스트로{주상복합}"
+          detailAddress="101동 703호"
+          buildingType="아파트"
+          area="34.5㎡"
+          tags={["풀옵션", "xx역 도보 n분", "대학교 인접"]}
+          liked={false}
+        />
+        <PropertyCard
+          itemId={2}
+          itemNumber={2}
+          imageUrl="/imgs/-.png"
+          transactionType="전세"
+          price="5억 3,000"
+          address="방배마에스트로{주상복합}"
+          detailAddress="101동 703호"
+          buildingType="아파트"
+          area="34.5㎡"
+          tags={["풀옵션", "xx역 도보 n분", "대학교 인접"]}
+          liked={true}
+          small={true}
+        />
+      </div>
+
+      <div className="flex min-h-screen flex-col items-center justify-center gap-1 p-8">
+        <p className="text-largeTitle">Large Title 텍스트</p>
+        <p className="text-title1">Title 1 텍스트</p>
+        <p className="text-title2">Title 2 텍스트</p>
+        <p className="text-title3">Title 3 텍스트</p>
+        <p className="text-title4">Title 4 텍스트</p>
+        <p className="text-subtitle1">Subtitle 1 텍스트</p>
+        <p className="text-subtitle2">Subtitle 2 텍스트</p>
+        <p className="text-subtitle3">Subtitle 3 텍스트</p>
+        <p className="text-subtitle4">Subtitle 4 텍스트</p>
+        <p className="text-body1">Body 1 텍스트</p>
+        <p className="text-body2">Body 2 텍스트</p>
+        <p className="text-body3">Body 3 텍스트</p>
+        <p className="text-caption1">Caption 1 텍스트</p>
+        <p className="text-caption2">Caption 2 텍스트</p>
+        <p className="text-caption3">Caption 3 텍스트</p>
+        <p className="text-footnote">Footnote 텍스트</p>
+      </div>
+    </>
   );
 }
