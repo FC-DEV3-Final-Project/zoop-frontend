8eb325053640252e3018d9be80a99cdb03bcbfec
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-05-31 23:05:36 +0900

    feat: 공통 컴포넌트 버튼

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,6 +1,8 @@
+import { Button } from "@/components/ui/button";
+
 export default function Test() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center gap-1 p-8">
+    <div className="flex flex-col items-center justify-center h-full gap-1 px-4">
       <p className="text-largeTitle">Large Title 텍스트</p>
       <p className="text-title1">Title 1 텍스트</p>
       <p className="text-title2">Title 2 텍스트</p>
@@ -17,6 +19,8 @@ export default function Test() {
       <p className="text-caption2">Caption 2 텍스트</p>
       <p className="text-caption3">Caption 3 텍스트</p>
       <p className="text-footnote">Footnote 텍스트</p>
+
+      <Button variant={"default"}>다음</Button>
     </div>
   );
 }
