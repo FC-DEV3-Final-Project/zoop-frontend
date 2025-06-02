04788805ac970c580b7edca034924921ad88732b
hgTime <hgtime@gmail.com>
2025-06-02 13:23:28 +0900

    feat: text들을 prop 전달 가능

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,7 +1,11 @@
+"use client";
 import BottomSheet from "@/components/BottomSheet";
 import { ChevronDown } from "lucide-react";
+import { useState } from "react";
 
 export default function Test() {
+  const [bottomSheetValue, setBottomSheetValue] = useState("");
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-1 p-8">
       <p className="text-largeTitle">Large Title 텍스트</p>
@@ -20,15 +24,20 @@ export default function Test() {
       <p className="text-caption2">Caption 2 텍스트</p>
       <p className="text-caption3">Caption 3 텍스트</p>
       <p className="text-footnote">Footnote 텍스트</p>
-
       {/* 버튼을 trigger로 넘기면 해당 버튼 클릭 시 바텀시트가 열립니다. */}
+      선택된 값 확인하기: {bottomSheetValue}
       <BottomSheet
         trigger={
-          <button className="flex items-center gap-[3px] rounded-[100px] border border-[#E4E4E4] px-3 py-1">
+          <button className="flex cursor-pointer items-center gap-[3px] rounded-[100px] border border-[#E4E4E4] px-3 py-1">
             가격순
             <ChevronDown className="h-3 w-3" />
           </button>
         }
+        title="정렬 방식"
+        items={[
+          { label: "가격 높은 순", value: "high" },
+          { label: "낮은 가격 순", value: "low" },
+        ]}
       />
     </div>
   );
--- src/components/BottomSheet.tsx
+++ src/components/BottomSheet.tsx
@@ -1,30 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+
+interface BottomSheetItem {
+  label: string;
+  value: string;
+}
 
 interface BottomSheetProps {
   trigger: React.ReactNode;
+  title: string;
+  items: BottomSheetItem[];
 }
 
-const BottomSheet = ({ trigger }: BottomSheetProps) => {
+const BottomSheet = ({ trigger, title, items }: BottomSheetProps) => {
+  const [open, setOpen] = useState(false);
   return (
     <div>
-      <Sheet>
+      <Sheet open={open} onOpenChange={setOpen}>
         <SheetTrigger asChild>{trigger}</SheetTrigger>
 
         <SheetContent>
           <SheetHeader>
             <SheetTitle className="text-body1 flex h-[40px] items-center justify-center">
-              필터 설정
+              {title}
             </SheetTitle>
           </SheetHeader>
 
           <div className="flex h-[110px] flex-col">
-            <button className="text-body1 flex h-[48px] cursor-pointer items-center justify-start px-[20px] text-left hover:bg-[#F4F4F4]">
-              가격 높은 순
-            </button>
-            <button className="text-body1 flex h-[48px] cursor-pointer items-center justify-start px-[20px] text-left hover:bg-[#F4F4F4]">
-              가격 낮은 순
-            </button>
+            {items.map((item) => (
+              <button
+                className="text-body1 flex h-[48px] cursor-pointer items-center justify-start px-[20px] text-left hover:bg-[#F4F4F4]"
+                key={item.value}
+              >
+                {item.label}
+              </button>
+            ))}
           </div>
         </SheetContent>
       </Sheet>
