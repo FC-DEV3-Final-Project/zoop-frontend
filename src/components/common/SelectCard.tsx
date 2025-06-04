6637c40432024168cf9aba93581e0fa3ae45c236
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-06-04 21:48:00 +0900

    feat: 라디오 버튼 공통 컴포넌트 분리

---
+++ src/components/common/SelectCard.tsx
@@ -0,0 +1,36 @@
+import clsx from "clsx";
+import React from "react";
+import Image from "next/image";
+
+interface SelectCardProps {
+  option: string;
+  selected: string;
+  setSelected: (option: string) => void;
+}
+
+const SelectCard = ({ option, selected, setSelected }: SelectCardProps) => {
+  const isSelected = selected === option;
+
+  return (
+    <button
+      key={option}
+      onClick={() => setSelected(option)}
+      className={clsx(
+        "shadow-customCard relative flex h-16 items-center justify-start rounded-[8px] pl-5 pr-4 text-subtitle1",
+        isSelected ? "bg-blue-100" : "bg-white",
+      )}
+    >
+      {option}
+      <span className="absolute right-4">
+        <Image
+          src={isSelected ? "/icons/check-on.svg" : "/icons/check-off.svg"}
+          alt={isSelected ? "선택됨" : "미선택"}
+          width={28}
+          height={28}
+        />
+      </span>
+    </button>
+  );
+};
+
+export default SelectCard;
