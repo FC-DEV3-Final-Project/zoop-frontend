963e6afb98504c650201541b543bce9a43dc03ab
jiyoon04 <94222592+jiyoon04@users.noreply.github.com>
2025-06-04 17:39:08 +0900

    feat: 탭 별 섹션 컴포넌트

--- src/app/property/[id]/components/detail/DetailTab.tsx
+++ src/app/property/[id]/components/detail/DetailTab.tsx
@@ -1,11 +1,13 @@
 import InfoBox from "./InfoBox";
 import ScrollableTabBar from "./ScrollableTabBar";
+import SectionList from "./SectionList";
 
 export default function PropertyDetailTab({ id }: { id: string }) {
   return (
     <div className="flex flex-col gap-2">
       <InfoBox />
       <ScrollableTabBar />
+      <SectionList />
     </div>
   );
 }
