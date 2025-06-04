1f56682042174691bbef47804a14a1b760a1ebb9
jiyoon04 <94222592+jiyoon04@users.noreply.github.com>
2025-06-04 10:53:08 +0900

    remove: form 파일 삭제

--- src/app/property/[id]/PropertyReviewForm.tsx
+++
@@ -1,8 +0,0 @@
-export default function PropertyReviewForm({ id }: { id: string }) {
-  return (
-    <div className="p-4">
-      <h2 className="text-xl font-semibold">매물 상세 정보</h2>
-      {/* 추후 fetch로 불러오거나 props로 넘기기 */}
-    </div>
-  );
-}
