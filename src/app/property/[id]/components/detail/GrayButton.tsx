8acca2e0a76dce042fc4b7d0685b4966a4c8fd2b
jiyoon04 <94222592+jiyoon04@users.noreply.github.com>
2025-06-05 12:16:24 +0900

    feat: 버튼 컴포넌트 제작

---
+++ src/app/property/[id]/components/detail/GrayButton.tsx
@@ -0,0 +1,22 @@
+interface GrayButtonProps {
+  label: string;
+  onClick?: () => void;
+  isToggle?: boolean;
+  isExpanded?: boolean;
+}
+
+export default function GrayButton({
+  label,
+  onClick,
+  isToggle = false,
+  isExpanded = false,
+}: GrayButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className="h-[50px] w-full rounded-small bg-gray-100 px-6 py-[9px] text-center text-body2 text-black"
+    >
+      {isToggle ? (isExpanded ? "접기" : "전체 설명 보기") : label}
+    </button>
+  );
+}
