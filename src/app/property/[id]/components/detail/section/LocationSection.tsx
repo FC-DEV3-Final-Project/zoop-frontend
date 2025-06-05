f08cd475975aea9a331e31a33b341291d1a7331b
jiyoon04 <94222592+jiyoon04@users.noreply.github.com>
2025-06-05 11:35:36 +0900

    feat: 학교 태그 가로스크롤에서 줄 바꿈 수정

--- src/app/property/[id]/components/detail/section/LocationSection.tsx
+++ src/app/property/[id]/components/detail/section/LocationSection.tsx
@@ -1,4 +1,6 @@
 export default function LocationSection() {
+  const nearbySchools = ["서문여자중학교(533m)", "이수중학교(533m)", "서문여자고등학교(533m)"];
+
   return (
     <section id="location" className="mb-2 scroll-mt-[80px] bg-white px-5 py-8">
       <div className="mb-5 text-title2 text-black">위치정보</div>
@@ -7,16 +9,15 @@ export default function LocationSection() {
       <div className="h-[210px] w-full rounded-small bg-gray-500"></div>
       <div className="mb-2 mt-5 text-subtitle1 text-black">주변 학교</div>
       <div className="-mx-4 overflow-x-auto px-4">
-        <div className="inline-flex gap-[10px]">
-          <span className="justify-center whitespace-nowrap rounded-[50px] bg-gray-200 px-2 py-1 align-middle">
-            서문여자중학교(533m)
-          </span>
-          <span className="justify-center whitespace-nowrap rounded-[50px] bg-gray-200 px-2 py-1 align-middle">
-            이수중학교(533m)
-          </span>
-          <span className="justify-center whitespace-nowrap rounded-[50px] bg-gray-200 px-2 py-1 align-middle">
-            서문여자고등학교(533m)
-          </span>
+        <div className="flex flex-wrap gap-[8px]">
+          {nearbySchools.map((school, idx) => (
+            <span
+              key={idx}
+              className="justify-center whitespace-nowrap rounded-[50px] bg-gray-200 px-2 py-1 align-middle"
+            >
+              {school}
+            </span>
+          ))}
         </div>
       </div>
     </section>
