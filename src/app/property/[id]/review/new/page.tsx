78318c003a0dd55b6f76ae593ae4427e8e01ccf6
jiyoon04 <94222592+jiyoon04@users.noreply.github.com>
2025-06-07 15:28:30 +0900

    feat: 리뷰 작성 페이지 컴포넌트 반영 및 하단 제출 버튼 위한 레이아웃 수정

---
+++ src/app/property/[id]/review/new/page.tsx
@@ -0,0 +1,70 @@
+// NewReviewPage.tsx
+"use client";
+
+import { useState } from "react";
+import SelectButtonGroup from "./components/SelectButtonGroup";
+import ReviewTextarea from "./components/ReviewTextarea";
+import StarRating from "./components/StarRating";
+import { Button } from "@/components/ui/button";
+
+export default function NewReviewPage() {
+  const [residence, setResidence] = useState("current");
+  const [hasChild, setHasChild] = useState("none");
+
+  return (
+    <div className="flex h-dvh flex-col bg-white">
+      {/* 본문 스크롤 영역 */}
+      <div className="flex-1 overflow-y-auto px-5 pb-[88px] pt-8">
+        <div className="mb-[30px] text-title2 text-blue-800-primary">
+          방배마에스트로[주상복합] 아파트
+        </div>
+
+        {/* 거주 여부 */}
+        <div className="mb-4 flex flex-col gap-2">
+          <div className="text-title4 text-black">거주 여부</div>
+          <SelectButtonGroup
+            selected={residence}
+            onChange={setResidence}
+            options={[
+              { label: "현재 거주", value: "current" },
+              { label: "과거 거주", value: "past" },
+              { label: "거주 안함", value: "none" },
+            ]}
+          />
+        </div>
+
+        {/* 자녀 유무 */}
+        <div className="mb-4 flex flex-col gap-2">
+          <div className="text-title4 text-black">자녀 유무</div>
+          <SelectButtonGroup
+            selected={hasChild}
+            onChange={setHasChild}
+            options={[
+              { label: "없음", value: "none" },
+              { label: "있음", value: "yes" },
+            ]}
+          />
+        </div>
+
+        {/* 리뷰 작성 */}
+        <div className="mb-[18px] flex flex-col gap-2">
+          <div className="text-title4 text-black">내용을 작성해주세요</div>
+          <ReviewTextarea />
+        </div>
+
+        {/* 별점 남기기 */}
+        <div className="flex flex-col gap-2">
+          <div className="text-title4 text-black">추천 점수 남기기</div>
+          <StarRating />
+        </div>
+      </div>
+
+      {/* 하단 버튼 */}
+      <div className="sticky bottom-0 left-0 w-full bg-white px-5 py-3">
+        <Button variant="default" className="w-full">
+          등록하기
+        </Button>
+      </div>
+    </div>
+  );
+}
