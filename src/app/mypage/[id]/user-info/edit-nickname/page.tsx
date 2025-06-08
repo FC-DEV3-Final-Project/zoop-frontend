2daab38431cc518a2b91f61ac225a9529ffd5f2c
Jang Eunhye <hh961130@naver.com>
2025-06-08 17:15:29 +0900

    design: 스타일 가이드 적용

--- src/app/mypage/[id]/user-info/edit-nickname/page.tsx
+++ src/app/mypage/[id]/user-info/edit-nickname/page.tsx
@@ -15,22 +15,18 @@ export default function EditNickname() {
   };
 
   return (
-    <div className="relative h-screen bg-white">
+    <div className="relative">
       <div className="absolute top-[61px] inline-flex w-full flex-col items-start justify-start gap-4 p-5">
-        <div className="justify-center self-stretch font-['Pretendard'] text-base font-semibold leading-snug text-black">
-          닉네임
-        </div>
-        <div className="inline-flex items-center justify-between self-stretch rounded-lg px-4 py-3 outline outline-1 outline-offset-[-1px] outline-gray-200">
+        <div className="justify-center text-subtitle2">닉네임</div>
+        <div className="flex items-center justify-between self-stretch rounded-small px-4 py-3 outline outline-1 outline-offset-[-1px] outline-gray-200">
           <input
             type="text"
             value={nickname}
             onChange={handleChange}
             placeholder="닉네임을 입력해주세요."
-            className="w-full text-sm text-gray-800 placeholder-gray-300 focus:outline-none"
+            className="w-full text-caption2 placeholder-gray-300 focus:outline-none"
           />
-          <div className="justify-center font-['Pretendard'] text-sm font-semibold leading-tight text-gray-300">
-            {nickname.length}/20
-          </div>
+          <div className="text-caption1 text-gray-300">{nickname.length}/20</div>
         </div>
       </div>
     </div>
--- src/app/mypage/[id]/user-info/edit-profile-image/page.tsx
+++ src/app/mypage/[id]/user-info/edit-profile-image/page.tsx
@@ -8,7 +8,7 @@ export default function EditProfileImage() {
   return (
     <div className="relative flex h-full flex-col">
       {/* 아이콘 */}
-      <button className="absolute right-5 top-3 h-6 w-6">
+      <button className="absolute right-5 top-3">
         <Image src="/icons/x.svg" alt="닫기" width={24} height={24} />
       </button>
       {/* 이미지 영역 */}
@@ -20,10 +20,10 @@ export default function EditProfileImage() {
       {/* 버튼 영역 */}
       <div className="flex h-14">
         <div className="flex flex-1 items-center justify-center">
-          <span className="text-base font-medium">변경하기</span>
+          <span className="text-body1">변경하기</span>
         </div>
         <div className="flex flex-1 items-center justify-center">
-          <span className="text-base font-medium">삭제하기</span>
+          <span className="text-body1">삭제하기</span>
         </div>
       </div>
     </div>
