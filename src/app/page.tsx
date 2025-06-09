5956e6709ecb572d67b77dc39bb94b78415b12db
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-06-09 17:28:36 +0900

    feat: home에 header 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+
+import Header from "@/layout/Header";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <h1 className="text-2xl font-bold">홈(채팅)페이지입니다</h1>
+    <div className="flex min-h-screen flex-col">
+      <Header
+        title="ZOOP"
+        onHamburgerClick={() => alert("히스토리 열기")}
+        onAlarmClick={() => alert("알림 열기")}
+        bgColorClassName="bg-gray-100"
+      />
+      <main className="flex min-h-screen items-center justify-center pt-16">
+        <h1 className="text-2xl font-bold">홈(채팅)페이지입니다</h1>
+      </main>
     </div>
   );
 }
