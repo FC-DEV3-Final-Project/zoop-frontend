47fff2d0c03f0d003552363e1d2df8e6cefbdf2b
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-06-09 11:43:44 +0900

    design: 매매 형태 설정 페이지 퍼블리싱

--- src/components/setup/TransactionTypeStep.tsx
+++ src/components/setup/TransactionTypeStep.tsx
@@ -1,4 +1,3 @@
-import clsx from "clsx";
 import React, { useState } from "react";
 import { Button } from "../ui/button";
 import SelectCard from "../common/SelectCard";
@@ -13,15 +12,25 @@ const TransactionTypeStep = ({ onNext }: TransactionTypeStepProps) => {
   const [selected, setSelected] = useState<string[]>([]);
 
   return (
-    <div>
-      <h1 className="text-[22px] font-medium">원하는 매매 형태를 알려주세요</h1>
-      <div className="mb-[10px] flex justify-end text-body2">중복 선택 가능</div>
-      <div className="flex flex-col gap-4">
-        {TRANSACTION_OPTIONS.map((option) => (
-          <SelectCard key={option} option={option} selected={selected} setSelected={setSelected} />
-        ))}
+    <div className="relative flex h-full flex-col gap-5">
+      <h1 className="text-[22px] font-medium">원하는 매매 형태를 선택해주세요.</h1>
+      <div className="flex flex-col gap-[10px]">
+        <div className="flex justify-end text-body2 text-gray-800">중복 선택 가능</div>
+        <div className="flex flex-col gap-4">
+          {TRANSACTION_OPTIONS.map((option) => (
+            <SelectCard
+              key={option}
+              option={option}
+              selected={selected}
+              setSelected={setSelected}
+            />
+          ))}
+        </div>
       </div>
-      <Button onClick={onNext}>다음</Button>
+
+      <div className="absolute bottom-4 w-full">
+        <Button onClick={onNext}>다음</Button>
+      </div>
     </div>
   );
 };
