b7f2712f60df22f7a19178d0d8260de21b3591c4
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-06-09 12:18:47 +0900

    design: 예산 설정 페이지 퍼블리싱

--- src/components/setup/BudgeStep.tsx
+++ src/components/setup/BudgeStep.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../ui/button";
 
 interface BudgeStepProps {
@@ -6,14 +6,38 @@ interface BudgeStepProps {
 }
 
 const BudgeStep = ({ onNext }: BudgeStepProps) => {
+  const [deposit, setDeposit] = useState(0); // 보증금
+  const [monthlyRent, setMonthlyRent] = useState(0); // 월세
+
   return (
-    <div>
+    <div className="relative flex h-full flex-col gap-5">
       <h1 className="text-[22px] font-medium">생각해 둔 예산을 알려주세요</h1>
-      <label>보증금</label>
-      <input className="w-full appearance-none border-b-[1px] border-gray-500 bg-transparent pb-2 outline-none placeholder:text-[20px]" />
-      <label>월세</label>
-      <input className="w-full appearance-none border-b-[1px] border-gray-500 bg-transparent pb-2 outline-none placeholder:text-[20px]" />
-      <Button>결과 확인하기</Button>
+      <div className="flex flex-col gap-1">
+        <div className="relative">
+          <label className="text-subtitle2 text-gray-800">보증금</label>
+          <input
+            className="w-full appearance-none border-b-[2px] border-gray-500-alternative bg-transparent py-2 outline-none placeholder:text-[20px]"
+            value={deposit}
+            onChange={(e) => setDeposit(Number(e.target.value))}
+          />
+          <div className="absolute right-0 top-7 text-[20px] font-medium">만원</div>
+          <div className="mt-[2px] flex w-full justify-end text-subtitle3 text-gray-800">0만원</div>
+        </div>
+        <div className="relative">
+          <label className="text-subtitle2 text-gray-800">월세</label>
+          <input
+            className="w-full appearance-none border-b-[2px] border-gray-500-alternative bg-transparent py-2 outline-none placeholder:text-[20px]"
+            value={monthlyRent}
+            onChange={(e) => setMonthlyRent(Number(e.target.value))}
+          />
+          <div className="absolute right-0 top-7 text-[20px] font-medium">만원</div>
+          <div className="mt-[2px] flex w-full justify-end text-subtitle3 text-gray-800">0만원</div>
+        </div>
+      </div>
+
+      <div className="absolute bottom-4 w-full">
+        <Button onClick={onNext}>결과 확인하기</Button>
+      </div>
     </div>
   );
 };
