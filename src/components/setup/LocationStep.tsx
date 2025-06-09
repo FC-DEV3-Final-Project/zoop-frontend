17f3b67d4a9f98006d265e53c29ba46de7737898
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-06-09 13:40:02 +0900

    feat: 지역 설정 페이지 검색 결과 영역 추가

--- src/components/setup/LocationStep.tsx
+++ src/components/setup/LocationStep.tsx
@@ -7,8 +7,40 @@ interface LocationStepProps {
   onNext: () => void;
 }
 
+// 임시
+const dummyData = [
+  { title: "이수역 7호선", detail: "서울 동작구 동작대로 105-2" },
+  { title: "이수역 7호선", detail: "서울 동작구 동작대로 104-2" },
+  { title: "이수역 7호선", detail: "서울 동작구 동작대로 105-2" },
+  { title: "이수역 7호선", detail: "서울 동작구 동작대로 108-2" },
+  { title: "이수역 7호선", detail: "서울 동작구 동작대로 105-1" },
+  { title: "이수역 7호선", detail: "서울 동작구 동작대로 105-1" },
+  { title: "이수역 7호선", detail: "서울 동작구 동작대로 105-1" },
+  { title: "이수역 7호선", detail: "서울 동작구 동작대로 105-1" },
+  { title: "이수역 7호선", detail: "서울 동작구 동작대로 105-1" },
+  { title: "이수역 7호선", detail: "서울 동작구 동작대로 105-1" },
+];
+
 const LocationStep = ({ onNext }: LocationStepProps) => {
   const [input, setInput] = useState("");
+  const [searchKeyword, setSearchKeyword] = useState("");
+
+  const handleSearch = () => {
+    if (input.trim()) {
+      setSearchKeyword(input.trim());
+    }
+
+    // TODO: API 통신
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
+
+  // 임시
+  const filteredData = dummyData.filter((item) => item.title.includes(searchKeyword));
 
   return (
     <div className="relative flex h-full flex-col gap-5">
@@ -19,9 +51,29 @@ const LocationStep = ({ onNext }: LocationStepProps) => {
           placeholder="지역, 지하철역, 단지, 건물명을 검색"
           value={input}
           onChange={(e) => setInput(e.target.value)}
+          onKeyDown={handleKeyDown}
         />
-        <Image src={SearchIcon} alt="검색" className="absolute right-0 top-2" />
+        <Image
+          src={SearchIcon}
+          alt="검색"
+          className="absolute right-0 top-2"
+          onClick={handleSearch}
+        />
       </div>
+
+      {searchKeyword && (
+        <div className="flex flex-col gap-4 overflow-hidden">
+          <div className="text-body2 text-gray-800">검색결과 ({filteredData.length})</div>
+          <ul className="flex h-[270px] flex-col gap-[6px] overflow-y-auto">
+            {filteredData.map((item, idx) => (
+              <li key={idx} className="py-[10px]">
+                <div className="text-body1">{item.title}</div>
+                <div className="text-body2">{item.detail}</div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
 
       <div className="absolute bottom-4 w-full">
         <Button onClick={onNext} disabled={input.length === 0}>
