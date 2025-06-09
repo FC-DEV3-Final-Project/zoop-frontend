c723feb5fbf6ef6a0159b9b05944ddd4c06d76cf
jiyoon04 <94222592+jiyoon04@users.noreply.github.com>
2025-06-09 17:47:48 +0900

    design: font size 수정

--- src/app/property/[id]/(with-tabs)/detail/components/GrayButton.tsx
+++ src/app/property/[id]/(with-tabs)/detail/components/GrayButton.tsx
@@ -16,7 +16,7 @@ export default function GrayButton({
   return (
     <button
       onClick={onClick}
-      className="h-[50px] w-full cursor-pointer rounded-small bg-gray-100 px-6 py-[9px] text-center text-body2 text-black"
+      className="h-[50px] w-full cursor-pointer rounded-small bg-gray-100 px-6 py-[9px] text-center text-caption2 text-black"
     >
       {isToggle ? (isExpanded ? "접기" : "전체 설명 보기") : label}
     </button>
