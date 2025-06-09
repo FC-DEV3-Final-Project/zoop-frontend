73fdd6043f2572c526bfc13fcfbdd3c3a8d56e73
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-06-09 11:35:16 +0900

    design: 지역 설정 페이지 퍼블리싱

--- src/components/setup/LocationStep.tsx
+++ src/components/setup/LocationStep.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Button } from "../ui/button";
+import SearchIcon from "../../../public/icons/search.svg";
+import Image from "next/image";
 
 interface LocationStepProps {
   onNext: () => void;
@@ -7,14 +9,19 @@ interface LocationStepProps {
 
 const LocationStep = ({ onNext }: LocationStepProps) => {
   return (
-    <div>
+    <div className="relative flex h-full flex-col gap-5">
       <h1 className="text-[22px] font-medium">원하는 지역을 알려주세요</h1>
-      <input
-        className="w-full appearance-none border-b-[1px] border-gray-500 bg-transparent pb-2 outline-none placeholder:text-[20px]"
-        placeholder="지역, 지하철역, 단지, 건물명을 검색"
-      />
+      <div className="relative">
+        <input
+          className="w-full appearance-none border-b-[1px] border-gray-500 bg-transparent pb-2 pt-2 outline-none placeholder:text-[20px]"
+          placeholder="지역, 지하철역, 단지, 건물명을 검색"
+        />
+        <Image src={SearchIcon} alt="검색" className="absolute right-0 top-2" />
+      </div>
 
-      <Button onClick={onNext}>다음</Button>
+      <div className="absolute bottom-4 w-full">
+        <Button onClick={onNext}>다음</Button>
+      </div>
     </div>
   );
 };
