8b4b7e0144deda6d3eb46dbb38854f16bfd8b7f8
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-06-13 04:07:40 +0900

    feat: home sidebar 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,43 @@
 "use client";
 
+import {
+  Sheet,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import { Header } from "@/layout/Header";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col">
+    <Sheet>
       <Header bgColorClassName="bg-gray-100">
-        <Header.Hamburger onHamburgerClick={() => alert("햄버거 클릭")} />
+        <SheetTrigger>
+          <Header.Hamburger />
+        </SheetTrigger>
         <Header.Title>ZOOP</Header.Title>
         <Header.Alarm onAlarmClick={() => alert("알림 클릭")} />
       </Header>
-
-      <main className="flex min-h-screen items-center justify-center pt-16">
-        <h1 className="text-2xl font-bold">홈(채팅)페이지입니다</h1>
-      </main>
-    </div>
+      <div className="flex flex-col min-h-screen">
+        <div className="fixed top-16 w-full border-t-[1px] border-gray-400" />
+        <main className="mt-16">
+          <h1 className="text-2xl font-bold">홈(채팅)페이지입니다</h1>
+        </main>
+      </div>
+      <SheetContent side="left">
+        <SheetHeader>
+          <SheetTitle>
+            <div>검색창</div>
+          </SheetTitle>
+          <div>새로운 대화 시작하기</div>
+        </SheetHeader>
+        <div>히스토리 목록</div>
+        <SheetFooter>
+          <div>내 프로필</div>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
   );
 }
