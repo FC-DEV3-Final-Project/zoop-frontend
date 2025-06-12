987130e99836062c959856bb758a7f0115c4b1fd
Jang-eunhye <hh961130@naver.com>
2025-06-12 12:52:06 +0900

    feat: 부동산페이지 추가 퍼블리싱 (#40)
    * feat: 부동산 페이지 퍼블리싱 1차
    
    * design: 부동산 정보영역 퍼블리싱
    
    * design: 탭, 리스트 영역 추가
    
    * feat: 더보기 버튼 수정
    
    * design: 버튼 추가
    
    * design: 버튼 뒤 흰 배경
    
    * design: 버튼 위치 수정
    
    * design: 버튼 속성 수정
    
    * design: 버튼 수정
    
    * design: 불필요한 클래스 제거
    
    * style: 변수명 수정(대소문자)
    
    * design: 부동산 정보영역 퍼블리싱 1차
    
    * design: 부동산영역 추가 퍼블리싱
    
    * feat: 데이터 분리, 패딩추가
    
    * rename: 불필요한 코드 제거 및 수정
    
    * rename: 불필요한 코드 제거
    
    * design: 헤더 추가

--- src/app/real-estate/[id]/page.tsx
+++ src/app/real-estate/[id]/page.tsx
@@ -1,13 +1,28 @@
 "use client";
 import PropertyListSection from "@/components/common/PropertyListSection";
-import { useRef, useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
+import RealEstateInfo from "@/components/real-estate/RealEstateInfo";
+import { Header } from "@/layout/Header";
+import { useRouter } from "next/navigation";
 
-const tabOptions = [
+const statsItems = [
   { label: "월세", value: "rent" },
   { label: "전세", value: "lease" },
   { label: "매매", value: "sale" },
 ];
+
+const realEstateData = {
+  name: "일등 부동산 공인중개사사무소",
+  representative: "김정순",
+  registrationNumber: "44862989",
+  phone: "031-271-5309, 010-8711-6151",
+  address:
+    "경기도 수원시 장안구 경수대로 1083 1층 경기도 수원시 장안구 경수대로 1083 1층 경기도 수원시 장안구 경수대로 1083 1층",
+  sale: 30,
+  lease: 4,
+  rent: 19,
+  statsItems,
+};
 
 const properties = [
   {
@@ -79,97 +94,25 @@ const properties = [
 ];
 
 export default function RealEstatePage({ params }: { params: { id: string } }) {
-  const textRef = useRef<HTMLDivElement>(null);
-  const [isTruncated, setIsTruncated] = useState(false);
-  const [isExpanded, setIsExpanded] = useState(false);
-
-  useEffect(() => {
-    const el = textRef.current;
-    if (!el) return;
-
-    const lineHeight = parseFloat(getComputedStyle(el).lineHeight);
-
-    if (el.scrollHeight > lineHeight + 1) {
-      setIsTruncated(true);
-    }
-  }, []);
-
+  const router = useRouter();
   return (
     <>
-      <div className="flex flex-col gap-3 bg-white p-5">
-        <div className="text-center text-xl font-semibold text-black">
-          일등 부동산 공인중개사사무소
-        </div>
-        <div className="flex gap-5">
-          <div className="flex w-24 flex-shrink-0 flex-col items-center gap-1">
-            <div className="h-24 w-full rounded-lg bg-black" />
-            <div className="flex w-full items-center justify-center gap-1">
-              <div className="text-xs font-medium text-black">대표</div>
-              <div className="text-sm font-semibold text-black">김정순</div>
-            </div>
-          </div>
-          <div className="flex flex-col gap-1">
-            <div className="flex h-24 flex-col gap-[3px]">
-              <div className="flex items-center gap-1">
-                <div className="text-xs font-medium text-black">등록번호</div>
-                <div className="flex items-center rounded-lg bg-zinc-100 px-2 py-0.5">
-                  <div className="text-xs font-medium text-black">44862989</div>
-                </div>
-              </div>
-              <div className="flex w-full items-start gap-2">
-                <div className="whitespace-nowrap text-xs font-medium text-black">주소</div>
-                <div
-                  ref={textRef}
-                  className={`pr-6 text-xs font-medium text-black transition-all ${
-                    isExpanded ? "line-clamp-none" : "line-clamp-1"
-                  }`}
-                >
-                  경기도 수원시 장안구 경수대로 1083 1층 경기도 수원시 장안구 경수대로 1083 1층
-                </div>
-                {isTruncated && (
-                  <img
-                    src={isExpanded ? "/icons/arrow-up.svg" : "/icons/arrow-down.svg"}
-                    alt={isExpanded ? "접기" : "더보기"}
-                    className="h-4 w-4 cursor-pointer"
-                    onClick={() => setIsExpanded(!isExpanded)}
-                  />
-                )}
-              </div>
-              <div className="flex items-center gap-2">
-                <div className="text-xs font-medium text-black">최근 3개월 집주인확인</div>
-                <div className="text-xs font-semibold text-black">169건</div>
-              </div>
-              <div className="flex items-center gap-2">
-                <div className="text-xs font-medium text-blue-700">영업중</div>
-                <div className="text-xs font-semibold text-black">10:00~</div>
-              </div>
-            </div>
-            <div className="flex w-full items-center gap-1">
-              <div className="flex items-center gap-0.5">
-                <div className="text-sm font-medium text-black">매매</div>
-                <div className="text-sm font-semibold text-blue-700">30</div>
-              </div>
-              <div className="flex items-center gap-0.5">
-                <div className="text-sm font-medium text-black">전세</div>
-                <div className="text-sm font-semibold text-blue-700">4</div>
-              </div>
-              <div className="flex items-center gap-0.5">
-                <div className="text-sm font-medium text-black">월세</div>
-                <div className="text-sm font-semibold text-blue-700">19</div>
-              </div>
-            </div>
-          </div>
-        </div>
+    <Header>
+      <Header.Prev onPrevClick={() => router.back()} />
+      <Header.Title>{realEstateData.name}</Header.Title>
+    </Header>
+      <RealEstateInfo {...realEstateData} />
+      <div className="pb-[76px]">
+        <PropertyListSection
+          showMapViewButton={false}
+          tabOptions={statsItems}
+          propertyMap={{
+            rent: properties,
+            lease: properties,
+            sale: properties,
+          }}
+        />
       </div>
-      <PropertyListSection
-        showMapViewButton={false}
-        tabOptions={tabOptions}
-        propertyMap={{
-          rent: properties,
-          lease: properties,
-          sale: properties,
-        }}
-      />
       {/* 공인중개사에게 전화 걸기 버튼 */}
       <div className="fixed bottom-0 left-1/2 w-full max-w-[600px] -translate-x-1/2 bg-white px-5 py-3">
         <Button variant="default">공인중개사에게 전화 걸기</Button>
