b3c20507c600bd0ceca3b15cceeb5c61c357c6dd
Jang Eunhye <hh961130@naver.com>
2025-06-12 20:34:26 +0900

    style: 마이페이지 컴포넌트 분리

--- src/components/mypage/PostPreview.tsx
+++ src/components/mypage/PostPreview.tsx
@@ -1,24 +1,40 @@
+import PostPreviewItem from "./PostPreviewItem";
+
 type PostPreviewProps = {
-  content: string;
-  likes: number;
-  comments: number;
+  posts: Array<{
+    reviweId?: string | number;
+    content: string;
+    likeCount: number;
+    commentCount: number;
+  }>;
+  onMorePosts: () => void;
 };
 
-export default function PostPreview({ content, likes, comments }: PostPreviewProps) {
+export default function PostPreview({ posts, onMorePosts }: PostPreviewProps) {
   return (
-    <div className="flex h-8 w-full items-center gap-6 py-[5px] pr-[3px]">
-      <div className="flex-1 truncate">{content}</div>
-
-      {/* 아이콘 영역*/}
-      <div className="flex items-center gap-1 text-body3 text-gray-600-hint">
-        <div className="flex w-[26px] gap-0.5">
-          <img src="/icons/chat-text-disabled.svg" alt="댓글" className="h-4 w-4" />
-          <span>{comments}</span>
-        </div>
-        <div className="flex w-[26px] gap-0.5">
-          <img src="/icons/thumbsup-disabled.svg" alt="좋아요" className="h-4 w-4" />
-          <span>{likes}</span>
-        </div>
+    <div className="shadow3 flex flex-col items-start justify-center gap-3.5 self-stretch rounded-lg bg-white px-5 py-4 outline outline-1 outline-offset-[-1px] outline-neutral-200">
+      <div className="inline-flex items-center justify-between self-stretch bg-white">
+        <div className="text-title4">내가 쓴 글</div>
+        <button onClick={onMorePosts} className="flex items-center gap-1">
+          <div className="text-body2 text-neutral-600">더보기</div>
+          <img src="/icons/arrow-right.svg" alt="더보기" className="h-4 w-4" />
+        </button>
+      </div>
+      <div className="flex flex-col items-start self-stretch">
+        {posts && posts.length > 0 ? (
+          posts.map((post, idx) => (
+            <PostPreviewItem
+              key={post.reviweId || idx}
+              content={post.content}
+              likes={post.likeCount}
+              comments={post.commentCount}
+            />
+          ))
+        ) : (
+          <div className="h-5 justify-center self-stretch text-body2 leading-tight">
+            내가 작성한 글이 없어요
+          </div>
+        )}
       </div>
     </div>
   );
---
+++ src/components/mypage/PostPreviewItem.tsx
@@ -0,0 +1,25 @@
+type PostPreviewItemProps = {
+  content: string;
+  likes: number;
+  comments: number;
+};
+
+export default function PostPreviewItem({ content, likes, comments }: PostPreviewItemProps) {
+  return (
+    <div className="flex h-8 w-full items-center gap-6 py-[5px] pr-[3px]">
+      <div className="flex-1 truncate">{content}</div>
+
+      {/* 아이콘 영역*/}
+      <div className="flex items-center gap-1 text-body3 text-gray-600-hint">
+        <div className="flex w-[26px] gap-0.5">
+          <img src="/icons/chat-text-disabled.svg" alt="댓글" className="h-4 w-4" />
+          <span>{comments}</span>
+        </div>
+        <div className="flex w-[26px] gap-0.5">
+          <img src="/icons/thumbsup-disabled.svg" alt="좋아요" className="h-4 w-4" />
+          <span>{likes}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
