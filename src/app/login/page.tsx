15aa265440f5e9c20b884d5a7f91d4da081f98f9
hgTime <hgtime@gmail.com>
2025-06-12 15:32:20 +0900

    design: 퍼블리싱 완료

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,17 @@
+"use client";
+import { Header } from "@/layout/Header";
 import Link from "next/link";
 
 export default function LoginPage() {
   return (
-    <div className="relative flex min-h-screen flex-col items-center justify-center">
-      <div className="absolute left-0 top-0 flex w-full items-center justify-between p-4">
-        <img src="/icons/arrow-left.svg" alt="뒤로가기" className="h-6 w-6" />
-        <img src="/icons/x.svg" alt="닫기" className="h-6 w-6" />
-      </div>
-      <div className="flex flex-col items-center gap-4">
+    <div className="relative flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#EDF0FD] to-white">
+      <Header bgColorClassName="bg-white/0">
+        <div className="w-6" />
+        <Header.Title>ZOOP</Header.Title>
+        <Header.Close onCloseClick={() => alert("알림 클릭")} />
+      </Header>
+
+      <div className="mt-[64px] flex flex-col items-center gap-4">
         {/* 로고 + 텍스트 */}
         <div className="flex flex-col items-center gap-1">
           <div className="flex items-center gap-[15px]">
@@ -16,31 +20,20 @@ export default function LoginPage() {
               ZOOP
             </span>
           </div>
-          <span className="text-center text-[18px] font-medium leading-[1.4] text-black">
+          <span className="mb-[173px] text-center text-[18px] font-medium leading-[1.4] text-black">
             부동산 매물 추천 AI 챗봇
           </span>
         </div>
 
         {/* 버튼 */}
-        <button className="flex h-[50px] w-[335px] items-center justify-center gap-2 rounded-[8px] bg-yellow-300 font-medium">
-          <img src="/icons/kakako-logo.svg" alt="카카오톡 로고" className="h-[18px] w-[18px]" />
-          카카오톡으로 로그인
-        </button>
-
-        {/* 링크 */}
-        <Link href="/about" className="text-sm text-blue-600 underline">
-          둘러보기
+        <Link
+          href={`https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}`}
+        >
+          <div className="flex h-[50px] w-[560px] items-center justify-center gap-2 rounded-[8px] bg-yellow-300 font-medium">
+            <img src="/icons/kakako-logo.svg" alt="카카오톡 로고" className="h-[18px] w-[18px]" />
+            카카오톡으로 로그인
+          </div>
         </Link>
-      </div>
-      <div className="relative w-full max-w-md">
-        <label className="absolute -top-2 left-3 bg-white px-1 text-sm">닉네임</label>
-
-        <input
-          id="nickname"
-          type="text"
-          className="w-full rounded-[12px] border px-4 py-3 outline-none focus:ring-2"
-          placeholder=""
-        />
       </div>
     </div>
   );
