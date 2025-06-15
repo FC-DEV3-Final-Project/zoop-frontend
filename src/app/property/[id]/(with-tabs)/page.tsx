9457d2f800fae89e5d16f25203cb686f454a21be
jiyoon04 <94222592+jiyoon04@users.noreply.github.com>
2025-06-15 17:11:26 +0900

    feat: PropertyDetailPage에 basicInfo useQuery 로직 추가

--- src/app/property/[id]/(with-tabs)/page.tsx
+++ src/app/property/[id]/(with-tabs)/page.tsx
@@ -5,23 +5,36 @@ import { useRouter } from "next/navigation";
 import InfoBox from "@/components/property/detail/InfoBox";
 import ScrollableSection from "@/components/property/detail/ScrollableSection";
 import { Header } from "@/layout/Header";
+import { useQuery } from "@tanstack/react-query";
+import { fetchBasicInfo } from "@/apis/property/detail/fetchBasicInfo";
 
-const PropertyDetailPage = ({ params }: { params: Promise<{ id: string }> }) => {
+export default function PropertyDetailPage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = use(params);
+  const propertyId = Number(id);
   const router = useRouter();
+
+  const {
+    data: basicInfo,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["basicInfo", propertyId],
+    queryFn: () => fetchBasicInfo(propertyId),
+  });
+
+  if (isLoading || error || !basicInfo) return null;
 
   return (
     <>
       <Header>
         <Header.Prev onPrevClick={() => router.back()} />
-        <Header.Title>방배</Header.Title>
+        <Header.Title>{basicInfo.articleName}</Header.Title>
       </Header>
+
       <div className="flex flex-col gap-2">
-        <InfoBox itemId={Number(id)} />
-        <ScrollableSection />
+        <InfoBox propertyInfo={basicInfo} />
+        <ScrollableSection propertyId={basicInfo.propertyId} />
       </div>
     </>
   );
-};
-
-export default PropertyDetailPage;
+}
