de1b6185371791800d59dafd009dd6cfdcaf1f84
Jang Eunhye <hh961130@naver.com>
2025-06-15 23:39:18 +0900

    fix: fetch 방식 수정

--- src/app/mypage/[id]/page.tsx
+++ src/app/mypage/[id]/page.tsx
@@ -8,70 +8,67 @@ import PropertyListSection from "@/components/common/PropertyListSection";
 import { useInfiniteScroll } from "@/hooks/common/useInfiniteScroll";
 import { PropertyCardProps } from "@/components/common/PropertyCard";
 
-// 1. 초기 데이터 (home API)
-const fetchInitialData = async () => {
-  const res = await fetch("/mypage/home");
-  const result = await res.json();
-  return {
-    profile: result.data.profile,
-    myReviews: result.data.myReviews,
-    initialBookmarkedProperties: result.data.bookmarkedProperties as PropertyCardProps[],
-    initialRecentViewedProperties: result.data.recentViewedProperties as PropertyCardProps[],
-  };
-};
-
-// 2. 추가 데이터 (bookmark API)
-const fetchBookmarkedItems = async (page: number) => {
-  await new Promise((resolve) => setTimeout(resolve, 1000)); // 1초 대기
-  const res = await fetch(`/mypage/bookmarked-properties?page=${page}`);
-  const result = await res.json();
-  return {
-    content: result.content as PropertyCardProps[],
-    hasNext: result.hasNext,
-  };
-};
-
 const MyPage = () => {
   const router = useRouter();
   const { id } = useParams();
 
   // home api 데이터 상태
-  const [data, setData] = useState<any>(null);
-  const [loading, setLoading] = useState(true);
+  const [homeData, setHomeData] = useState<any>(null);
+  const [homeLoading, setHomeLoading] = useState(true);
 
-  // data 구조분해할당
-  const {
-    profile,
-    myReviews = [],
-    initialBookmarkedProperties = [],
-    initialRecentViewedProperties = [],
-  } = data || {};
-
-  // home api 데이터 로드
+  // 1. 초기 데이터 (home API)
   useEffect(() => {
-    const loadInitialData = async () => {
+    const fetchInitialData = async () => {
       try {
-        const data = await fetchInitialData();
-        setData(data);
-      } catch (e) {
-        console.error("초기 데이터 로드 실패:", e);
-      } finally {
-        setLoading(false);
+        const homeResponse = await fetch("/mypage/home");
+        const homeData = await homeResponse.json();
+
+        // data 구조분해할당
+        const {
+          profile,
+          myReviews = [],
+          bookmarkedProperties = [],
+          recentViewedProperties = [],
+        } = homeData.data || {};
+
+        setHomeData({
+          profile,
+          myReviews,
+          initialBookmarkedProperties: bookmarkedProperties,
+          initialRecentViewedProperties: recentViewedProperties,
+        });
+
+        setHomeLoading(false);
+      } catch (error) {
+        console.error("초기 데이터 로드 실패:", error);
+        return null;
       }
     };
-    loadInitialData();
+
+    fetchInitialData();
   }, []);
 
-  // 추가 데이터 로드 (찜 무한스크롤)
+  // 2. 추가 데이터 (bookmark API)
+  const fetchBookmarkedItems = async (page: number) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000)); // 2초 대기
+    const res = await fetch(`/mypage/bookmarked-properties?page=${page}`);
+    const result = await res.json();
+    return {
+      content: result.content as PropertyCardProps[],
+      hasNext: result.hasNext,
+    };
+  };
+
+  // 무한 스크롤 훅 사용
   const {
-    items: bookmarkedMoreItems,
-    loader: bookmarkedLoader,
-    hasMore: bookmarkedHasMore,
+    items: additionalItems,
+    loader,
+    hasMore,
     loading: bookmarkedLoading,
   } = useInfiniteScroll<PropertyCardProps>(fetchBookmarkedItems, []);
 
   // 최종 리스트 (초기 데이터 + 추가 데이터)
-  const bookmarkedItems = [...(initialBookmarkedProperties || []), ...(bookmarkedMoreItems || [])];
+  const bookmarkedItems = [...(homeData?.initialBookmarkedProperties || []), ...additionalItems];
 
   const tabOptions = [
     { label: "찜한 매물", value: "liked" },
@@ -93,16 +90,16 @@ const MyPage = () => {
         <Header.Alarm onAlarmClick={() => alert("알림 클릭")} />
       </Header>
       <div className="flex h-screen flex-col bg-white pt-16">
-        {loading ? (
+        {homeLoading ? (
           <div>로딩중...</div>
         ) : (
           <>
             {/* 상단: 프로필/포스트 */}
             <section className="flex inline-flex flex-col items-start justify-start gap-6 bg-white px-5 pb-6 pt-7">
               {/* 유저 정보 */}
-              {profile && <UserProfile profile={profile} onEdit={handleEdit} />}
+              {homeData.profile && <UserProfile profile={homeData.profile} onEdit={handleEdit} />}
               {/* 포스트 박스 */}
-              <PostPreview posts={myReviews} onMorePosts={handleMorePosts} />
+              <PostPreview posts={homeData.myReviews} onMorePosts={handleMorePosts} />
             </section>
 
             {/* 하단: 탭바 + 리스트 */}
@@ -111,13 +108,13 @@ const MyPage = () => {
               isNumberVisible={false}
               propertyMap={{
                 liked: bookmarkedItems as PropertyCardProps[],
-                recent: initialRecentViewedProperties as PropertyCardProps[],
+                recent: (homeData?.initialRecentViewedProperties as PropertyCardProps[]) || [],
               }}
               loaders={{
-                liked: bookmarkedLoader,
+                liked: loader,
               }}
               hasMore={{
-                liked: bookmarkedHasMore,
+                liked: hasMore,
               }}
               loading={{
                 liked: bookmarkedLoading,
