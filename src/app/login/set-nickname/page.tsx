17c2af4e3f187fe06f6f83a66d5295cc89943d14
hgTime <hgtime@gmail.com>
2025-06-18 01:27:34 +0900

    design: nickNameInput 컴포넌트 적용

--- src/app/login/set-nickname/page.tsx
+++ src/app/login/set-nickname/page.tsx
@@ -2,14 +2,15 @@
 import React, { useEffect, useState } from "react";
 import { Header } from "@/layout/Header";
 import { Button } from "@/components/ui/button";
-import { createUserNickname } from "@/apis/login/user";
+import { createUserNickname } from "@/apis/login/createUserNickname";
 import { useRouter } from "next/navigation";
 import useAuthFromHash from "@/hooks/common/useAuthFromHash";
+import NicknameInput from "@/components/common/NicknameInput";
 
 const Page = () => {
   const router = useRouter();
   const [nickname, setNickname] = useState("");
-  const [loading, setLoading] = useState(false);
+  const [isValid, setIsValid] = useState(false);
 
   useAuthFromHash();
 
@@ -49,43 +50,25 @@ const Page = () => {
         <Header.Title>ZOOP</Header.Title>
         <Header.Close onCloseClick={() => alert("닫기 클릭")} />
       </Header>
-      {/* <div className="flex min-h-screen w-full flex-col items-center justify-center gap-5 px-5 pt-16"> */}
       <div className="flex h-screen w-full flex-col items-center justify-center bg-white px-5 pt-16">
         <span className="text-center text-title5">닉네임을 설정해주세요</span>
 
         <div
           className={`flex items-center justify-between self-stretch rounded-small px-4 py-3 outline outline-1 outline-offset-[-1px] outline-gray-500-alternative`}
         >
-          <input
-            type="text"
-            value={nickname}
-            onChange={(e) => {
-              setNickname(e.target.value);
-            }}
-            maxLength={20}
-            placeholder="닉네임을 입력해주세요. (최대 20자)"
-            className="w-full bg-white/0 text-body1 placeholder-gray-300 focus:outline-none"
+          <NicknameInput
+            nickname={nickname}
+            setNickname={setNickname}
+            isValid={isValid}
+            setIsValid={setIsValid}
           />
-          <button
-            onClick={() => alert("중복확인 클릭")}
-            disabled={nickname.length === 0 || loading}
-            className={`inline-flex items-center justify-center rounded-small px-4 py-1 outline outline-1 outline-offset-[-1px] ${
-              nickname.length > 0
-                ? "bg-blue-050-bg text-blue-700 outline-blue-800-primary"
-                : "bg-gray-200 text-gray-700-info outline-gray-500-alternative"
-            }`}
-          >
-            <p className="justify-start whitespace-nowrap text-caption1">
-              {loading ? "확인 중..." : "중복확인"}
-            </p>
-          </button>
         </div>
         {/* 버튼 */}
         <Button
           variant={"default"}
           className="mt-32 w-full"
           onClick={handleSubmit}
-          disabled={nickname.length === 0 || loading}
+          disabled={!isValid}
         >
           완료
         </Button>
