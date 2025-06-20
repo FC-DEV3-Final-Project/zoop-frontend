6a383dd8d44072268f7052b104ee1d01df1b9efb
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-06-20 11:05:03 +0900

    fix: import 경로 수정

--- src/components/filter/OptionSelectStep.tsx
+++ src/components/filter/OptionSelectStep.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Button } from "../ui/button";
-import FilterOptionCard from "../common/FilterOptionCard";
+import FilterOptionCard from "./FilterOptionCard";
 
 interface OptionSelectStepProps {
   onNext: () => void;
