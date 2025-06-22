f9f58d94f4ba6515ad27e98d2167a1a4f2424d3c
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-06-23 02:17:41 +0900

    feat: 명세서가 수정됨에 따라 API 호출 함수 및 훅 코드 수정

--- src/apis/sidebar/fetchChatList.ts
+++ src/apis/sidebar/fetchChatList.ts
@@ -1,8 +1,12 @@
 import axiosInstance from "../utils/axiosInstance";
 
-export const fetchChatList = async () => {
+type FetchChatListPayload = {
+  searchText: string;
+};
+
+export const fetchChatList = async ({ searchText }: FetchChatListPayload) => {
   try {
-    const response = await axiosInstance.get("/chats");
+    const response = await axiosInstance.post("/chats", { searchText });
     return response.data;
   } catch (error) {
     console.error("채팅 목록 불러오기 에러:", error);
--- src/queries/chat/useChatListQuery.ts
+++ src/queries/chat/useChatListQuery.ts
@@ -1,6 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchChatList } from "@/apis/sidebar/fetchChatList";
 
-export const useChatListQuery = () => {
-  return useQuery({ queryKey: ["chatList"], queryFn: fetchChatList });
+export const useChatListQuery = (searchText: string) => {
+  return useQuery({
+    queryKey: ["chatList", searchText],
+    queryFn: () => fetchChatList({ searchText }),
+    enabled: !!searchText,
+  });
 };
