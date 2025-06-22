0866fe92a8a712072ac66fda69b038c619eb547a
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-06-23 01:17:07 +0900

    feat: header size 속성 추가

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -10,12 +10,13 @@ import AlarmIcon from "../../public/icons/alram.svg";
 type HeaderRootProps = {
   children: React.ReactNode;
   bgColorClassName?: string;
+  size?: "sm" | "md";
 };
 
-const Root = ({ children, bgColorClassName = "bg-white" }: HeaderRootProps) => {
+const Root = ({ children, bgColorClassName = "bg-white", size = "sm" }: HeaderRootProps) => {
   return (
     <div
-      className={`fixed top-0 z-10 flex h-16 w-full max-w-[600px] items-center justify-between px-5 ${bgColorClassName}`}
+      className={`fixed top-0 z-10 flex w-full max-w-[600px] items-center justify-between px-5 ${bgColorClassName} ${size === "sm" ? "h-12" : "h-16"}`}
     >
       {children}
     </div>
@@ -29,13 +30,13 @@ const Prev = ({ onPrevClick }: { onPrevClick?: () => void }) => (
 );
 
 const Hamburger = ({ onHamburgerClick }: { onHamburgerClick?: () => void }) => (
-  <div onClick={onHamburgerClick} className="flex justify-center cursor-pointer">
+  <div onClick={onHamburgerClick} className="flex cursor-pointer justify-center">
     <Image src={HamburgerIcon} alt="메뉴" width={24} height={24} />
   </div>
 );
 
 const Title = ({ children }: { children: string }) => (
-  <h1 className="absolute -translate-x-1/2 left-1/2 text-subtitle2">{children}</h1>
+  <h1 className="absolute left-1/2 -translate-x-1/2 text-subtitle2">{children}</h1>
 );
 
 const Close = ({ onCloseClick }: { onCloseClick?: () => void }) => (
