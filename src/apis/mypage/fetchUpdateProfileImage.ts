06584b37be8192ad1af091cf22222ef6f56e4ca6
Jang Eunhye <hh961130@naver.com>
2025-06-23 15:14:45 +0900

    fix: 프로필이미지 키값 오류 해결

--- src/apis/mypage/fetchUpdateProfileImage.ts
+++ src/apis/mypage/fetchUpdateProfileImage.ts
@@ -2,7 +2,7 @@ import axiosInstance from "@/apis/utils/axiosInstance";
 
 const fetchUpdateProfileImage = async (file: File): Promise<string> => {
   const formData = new FormData();
-  formData.append("profileImage", file);
+  formData.append("profileImageUrl", file);
   const response = await axiosInstance.post("/mypage/profile-image", formData, {
     headers: {
       "Content-Type": "multipart/form-data",
