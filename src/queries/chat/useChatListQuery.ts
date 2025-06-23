b919c2dadc13fb53bea560ea070f9e9cfc91a176
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-06-24 01:27:50 +0900

    feat: 채팅 목록 조회 훅에 응답 타입 추가

--- src/queries/chat/useChatListQuery.ts
+++ src/queries/chat/useChatListQuery.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import axiosInstance from "@/apis/utils/axiosInstance";
+import { ChatPreviewItem } from "@/types/chat";
 
 type FetchChatListPayload = {
   searchText: string;
@@ -11,7 +12,7 @@ export const fetchChatList = async ({ searchText }: FetchChatListPayload) => {
 };
 
 export const useChatListQuery = (searchText: string) => {
-  return useQuery({
+  return useQuery<ChatPreviewItem[]>({
     queryKey: ["chatList", searchText],
     queryFn: () => fetchChatList({ searchText }),
   });
