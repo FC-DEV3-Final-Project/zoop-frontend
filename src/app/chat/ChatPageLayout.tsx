83233d018e93538bc3756ab8cfec151d8fdd2ee6
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-06-24 01:19:03 +0900

    feat: 기존 특정 채팅 불러오기 로직 추가

--- src/app/chat/ChatPageLayout.tsx
+++ src/app/chat/ChatPageLayout.tsx
@@ -1,214 +1,15 @@
-// components/chat/ChatPageLayout.tsx
 "use client";
 
 import { useEffect, useState } from "react";
+
 import { Header } from "@/layout/Header";
+import { Message } from "@/types/chat";
+
+import { Sheet, SheetTrigger } from "@/components/ui/sheet";
 import ChatMain from "@/components/chat/ChatMain";
 import SideBar from "@/components/chat/SideBar";
-import { Message, Property } from "@/types/chat";
-import { Sheet, SheetTrigger } from "@/components/ui/sheet";
 
-// 임시
-const dummyPropertyData: Property[] = [
-  {
-    order: 1,
-    propertyId: 98,
-    tradeTypeName: "월세",
-    rentPrice: 60,
-    warrantPrice: 1000,
-    dealPrice: 0,
-    dealOrWarrantPrc: "1,000",
-    tagList: ["25년이상", "올수리", "소형평수", "방두개"],
-    articleName: "다가구",
-    realEstateTypeName: "단독/다가구",
-    netArea: 32,
-    imageUrl: null,
-  },
-  {
-    order: 2,
-    propertyId: 109,
-    tradeTypeName: "월세",
-    rentPrice: 60,
-    warrantPrice: 1000,
-    dealPrice: 0,
-    dealOrWarrantPrc: "1,000",
-    tagList: ["25년이상", "소형평수", "방세개", "화장실한개"],
-    articleName: "단독",
-    realEstateTypeName: "단독/다가구",
-    netArea: 31,
-    imageUrl: null,
-  },
-  {
-    order: 3,
-    propertyId: 149,
-    tradeTypeName: "월세",
-    rentPrice: 60,
-    warrantPrice: 1000,
-    dealPrice: 0,
-    dealOrWarrantPrc: "500",
-    tagList: ["25년이상", "역세권", "방두개", "화장실한개"],
-    articleName: "단독",
-    realEstateTypeName: "단독/다가구",
-    netArea: 45,
-    imageUrl: null,
-  },
-  {
-    order: 4,
-    propertyId: 411,
-    tradeTypeName: "월세",
-    rentPrice: 60,
-    warrantPrice: 1000,
-    dealPrice: 0,
-    dealOrWarrantPrc: "1,000",
-    tagList: ["25년이상", "소형평수", "방세개", "화장실한개"],
-    articleName: "단독",
-    realEstateTypeName: "단독/다가구",
-    netArea: 31,
-    imageUrl: null,
-  },
-  {
-    order: 5,
-    propertyId: 412,
-    tradeTypeName: "월세",
-    rentPrice: 60,
-    warrantPrice: 1000,
-    dealPrice: 0,
-    dealOrWarrantPrc: "1,000",
-    tagList: ["25년이상", "융자금없는", "소형평수", "화장실한개"],
-    articleName: "다가구",
-    realEstateTypeName: "단독/다가구",
-    netArea: 18,
-    imageUrl:
-      "https://landthumb-phinf.pstatic.net/20250610_278/1749536795090vzlUC_JPEG/13813312_20232710134217.jpg",
-  },
-  {
-    order: 6,
-    propertyId: 413,
-    tradeTypeName: "월세",
-    rentPrice: 60,
-    warrantPrice: 1000,
-    dealPrice: 0,
-    dealOrWarrantPrc: "1,000",
-    tagList: ["25년이상", "소형평수", "방한개", "화장실한개"],
-    articleName: "다가구",
-    realEstateTypeName: "단독/다가구",
-    netArea: 20,
-    imageUrl: null,
-  },
-  {
-    order: 7,
-    propertyId: 414,
-    tradeTypeName: "월세",
-    rentPrice: 60,
-    warrantPrice: 1000,
-    dealPrice: 0,
-    dealOrWarrantPrc: "1,000",
-    tagList: ["25년이상", "융자금적은", "소형평수", "방한개"],
-    articleName: "다가구",
-    realEstateTypeName: "단독/다가구",
-    netArea: 21,
-    imageUrl: null,
-  },
-  {
-    order: 8,
-    propertyId: 415,
-    tradeTypeName: "월세",
-    rentPrice: 60,
-    warrantPrice: 1000,
-    dealPrice: 0,
-    dealOrWarrantPrc: "1,000",
-    tagList: ["25년이상", "화장실한개", "소형평수", "방한개"],
-    articleName: "다가구",
-    realEstateTypeName: "단독/다가구",
-    netArea: 28,
-    imageUrl: null,
-  },
-  {
-    order: 9,
-    propertyId: 416,
-    tradeTypeName: "월세",
-    rentPrice: 60,
-    warrantPrice: 1000,
-    dealPrice: 0,
-    dealOrWarrantPrc: "1,000",
-    tagList: ["25년이상", "화장실한개", "소형평수", "방한개"],
-    articleName: "다가구",
-    realEstateTypeName: "단독/다가구",
-    netArea: 19,
-    imageUrl: null,
-  },
-  {
-    order: 10,
-    propertyId: 417,
-    tradeTypeName: "월세",
-    rentPrice: 60,
-    warrantPrice: 1000,
-    dealPrice: 0,
-    dealOrWarrantPrc: "1,000",
-    tagList: ["25년이상", "융자금적은", "화장실한개", "소형평수"],
-    articleName: "다가구",
-    realEstateTypeName: "단독/다가구",
-    netArea: 19,
-    imageUrl: null,
-  },
-];
-const dummyMessageData: Message[] = [
-  {
-    messageId: 163,
-    senderType: "CHATBOT",
-    content: "지윤님의 조건에 알맞는 10개의 새로운 매물을 발견했습니다.",
-    createdAt: "2025-06-17T11:21:21.683968",
-    properties: dummyPropertyData,
-  },
-  {
-    messageId: 164,
-    senderType: "CHATBOT",
-    content: "추가하고 싶은 다른 조건이 있으신가요?",
-    createdAt: "2025-06-17T11:21:21.683970",
-  },
-  {
-    messageId: 165,
-    senderType: "USER",
-    content: "관공서가 가까운 곳을 추천해줘",
-    createdAt: "2025-06-17T11:24:21.683968",
-  },
-  {
-    messageId: 166,
-    senderType: "CHATBOT",
-    content: "해당 조건에 일치하는 매물을 찾지 못하였습니다. 다른 조건을 입력해보세요.",
-    createdAt: "2025-06-17T11:24:40.015175",
-  },
-  {
-    messageId: 167,
-    senderType: "USER",
-    content: "그럼 병원이 가까운 곳으로 찾아봐",
-    createdAt: "2025-06-17T11:24:40.484026",
-  },
-  {
-    messageId: 168,
-    senderType: "CHATBOT",
-    content: "해당 조건에 일치하는 매물을 찾지 못하였습니다. 다른 조건을 입력해보세요.",
-    createdAt: "2025-06-17T11:25:40.015175",
-  },
-  {
-    messageId: 169,
-    senderType: "USER",
-    content: "그럼 학교가 가까운 곳으로 찾아봐",
-    createdAt: "2025-06-17T11:26:40.484026",
-  },
-  {
-    messageId: 170,
-    senderType: "CHATBOT",
-    content: "해당 조건에 일치하는 매물을 찾지 못하였습니다. 다른 조건을 입력해보세요.",
-    createdAt: "2025-06-17T11:27:40.015175",
-  },
-  {
-    messageId: 171,
-    senderType: "USER",
-    content: "그럼 경찰서가 가까운 곳으로 찾아봐",
-    createdAt: "2025-06-17T11:28:40.484026",
-  },
-];
+import { useChatDataQuery } from "@/queries/chat/useChatDataQuery";
 
 interface ChatPageLayoutProps {
   selectedChatId: number | null;
@@ -219,17 +20,21 @@ const ChatPageLayout = ({ selectedChatId }: ChatPageLayoutProps) => {
   const [currentChatTitle, setCurrentChatTitle] = useState("ZOOP");
   const [currentChatMessages, setCurrentChatMessages] = useState<Message[]>([]);
 
-  useEffect(() => {
-    if (selectedChatId === null) {
-      setCurrentChatTitle("ZOOP");
-      setCurrentChatMessages([]);
-    } else {
-      // TODO: selectedChatId에 해당하는 채팅 불러오기 API 호출
+  // selectedChatId이 있다면 기존의 특정 채팅 불러오기
+  const { data: chatData } =
+    selectedChatId !== null ? useChatDataQuery(selectedChatId) : { data: null };
 
-      setCurrentChatTitle("강남역 / 월세 / 원룸 ⦁ 투룸 / 80"); // 임시
-      setCurrentChatMessages(dummyMessageData); // 임시
-    }
-  }, [selectedChatId]);
+  useEffect(() => {
+    if (!chatData) return;
+
+    setCurrentChatTitle(chatData.title || "ZOOP");
+
+    // 메세지 생성 순으로 오름차순
+    const sortedMessages = [...chatData.messages].sort(
+      (a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime(),
+    );
+    setCurrentChatMessages(sortedMessages);
+  }, [chatData]);
 
   return (
     <Sheet open={sideBarOpen} onOpenChange={setSideBarOpen}>
---
+++ src/queries/chat/useChatDataQuery.ts
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import axiosInstance from "@/apis/utils/axiosInstance";
+
+type FetchChatDataPayload = {
+  chatRoomId: number;
+};
+
+export const fetchChatData = async ({ chatRoomId }: FetchChatDataPayload) => {
+  const response = await axiosInstance.post(`/chats/${chatRoomId}`);
+  return response.data;
+};
+
+export const useChatDataQuery = (chatRoomId: number) => {
+  return useQuery({
+    queryKey: ["chatData", chatRoomId],
+    queryFn: () => fetchChatData({ chatRoomId }),
+    enabled: !!chatRoomId,
+  });
+};
