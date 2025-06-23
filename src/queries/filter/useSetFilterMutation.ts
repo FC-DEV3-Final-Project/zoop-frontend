2d6609813201f3b4573b17024e401250ff873484
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-06-24 00:01:05 +0900

    feat: 필터 설정하는 API 명세서에 따라 호출 로직 수정

--- src/queries/filter/useSetFilterMutation.ts
+++ src/queries/filter/useSetFilterMutation.ts
@@ -3,7 +3,9 @@ import axiosInstance from "@/apis/utils/axiosInstance";
 import { FilterPayload } from "@/types/filter";
 
 export const postChatFilter = async (payload: FilterPayload) => {
-  const response = await axiosInstance.post("/chats/filters", payload);
+  const { chatRoomId, filters } = payload;
+
+  const response = await axiosInstance.post(`/chats/filters/${chatRoomId}`, filters);
   return response.data;
 };
 
