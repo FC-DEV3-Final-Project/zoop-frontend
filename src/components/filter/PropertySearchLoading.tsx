7f69dd62993fd4043dc8701464a674f103ea59de
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-06-24 01:36:39 +0900

    style: text style 변경

--- src/components/filter/PropertySearchLoading.tsx
+++ src/components/filter/PropertySearchLoading.tsx
@@ -1,14 +1,13 @@
-import { Header } from "@/layout/Header";
-import { useUserInfoStore } from "@/stores/useUserInfoStore";
 import React from "react";
+import { useUserInfoStore } from "@/stores/useUserInfoStore";
 
 const PropertySearchLoading = () => {
   const { user } = useUserInfoStore();
 
   return (
-    <div className="flex flex-col items-center justify-center h-full">
-      <div className="flex items-center justify-center h-full mb-10">
-        <p className="text-center text-subtitle2">
+    <div className="flex h-full flex-col items-center justify-center">
+      <div className="mb-10 flex h-full items-center justify-center">
+        <p className="text-center text-title6">
           {user?.nickname}님의 조건에 <br /> 알맞는 매물을 찾고 있습니다
         </p>
       </div>
