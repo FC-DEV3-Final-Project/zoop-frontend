db2523f14943070d26acc88ab9e90a7ebb9683f8
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-06-25 09:42:13 +0900

    feat: 결과 확인하기 로딩 화면에 로딩 애니메이션 추가

--- src/components/filter/PropertySearchLoading.tsx
+++ src/components/filter/PropertySearchLoading.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { useUserInfoStore } from "@/stores/useUserInfoStore";
+import LoadingDots from "../common/LoadingDots";
 
 const PropertySearchLoading = () => {
   const { user } = useUserInfoStore();
 
   return (
     <div className="flex flex-col items-center justify-center h-full">
-      <div className="flex items-center justify-center h-full mb-10">
+      <div className="flex flex-col items-center justify-center h-full gap-5 mb-10">
+        <LoadingDots />
         <p className="text-center text-title6">
           {user?.nickname}님의 조건에 <br /> 알맞는 매물을 찾고 있습니다
         </p>
       </div>
-      {/** loading lottie */}
     </div>
   );
 };
