014bfbecf5615d379ec9f33eebf74f89fae0efdc
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-06-25 17:22:45 +0900

    feat: 채팅 내 지도 뷰어 및 다이얼로그 연결

--- src/components/chat/RecommendationCard/RecommendationHeader.tsx
+++ src/components/chat/RecommendationCard/RecommendationHeader.tsx
@@ -5,16 +5,19 @@ import Image from "next/image";
 
 import ArrowRight from "../../../../public/icons/arrow-right-gray.svg";
 
-const RecommendationHeader = () => {
+interface RecommendationHeaderProps {
+  setIsMapOpen: (isMapOpen: boolean) => void;
+}
+
+const RecommendationHeader = ({ setIsMapOpen }: RecommendationHeaderProps) => {
   const router = useRouter();
 
   return (
     <div className="flex justify-between rounded-b-none rounded-t-2xl bg-blue-050-bg px-3 py-[10px]">
       <h1 className="text-subtitle4 text-blue-800-primary">AI 추천 매물</h1>
       <span className="flex items-center justify-center gap-1">
-        {/** TODO: 자세히 보기 경로 수정 필요 */}
         <button
-          onClick={() => router.push("/")}
+          onClick={() => setIsMapOpen(true)}
           className="flex items-center text-center text-body3 leading-none text-gray-800"
         >
           자세히 보기
