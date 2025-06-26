57b95b8647ff5b7e298c0e01669ee7004fd4b828
Jang Eunhye <hh961130@naver.com>
2025-06-26 12:43:12 +0900

    fix: 로그아웃/탈퇴 쿠키방식으로 수정

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,6 +1,10 @@
 const clearAuthTokens = () => {
-  localStorage.removeItem("access_token");
-  localStorage.removeItem("refresh_token");
+  // 쿠키 삭제 (대문자 토큰명 사용)
+  document.cookie = "ACCESS_TOKEN=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  document.cookie = "REFRESH_TOKEN=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  document.cookie = "KAKAO_ACCESS=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+
+  // 기존 localStorage 데이터도 정리 (혹시 남아있을 수 있으므로)
   localStorage.removeItem("kakao_access");
   localStorage.removeItem("userInfo-storage");
 };
