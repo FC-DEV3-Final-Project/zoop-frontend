2479ae611a7e89800b77a25d3f5c7b2d620c3c9b
Jang Eunhye <hh961130@naver.com>
2025-06-26 11:56:39 +0900

    fix: 로그인 정보 확인 로직 수정

--- src/components/AuthGuard.tsx
+++ src/components/AuthGuard.tsx
@@ -1,32 +1,24 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import axiosInstance from "@/apis/utils/axiosInstance";
 
 const AuthGuard = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
-  const [checked, setChecked] = useState(false); // 로그인 체크 완료 여부
 
   useEffect(() => {
-    const checkAuth = async () => {
-      try {
-        await axiosInstance.get("/users/auth/me"); // 서버가 쿠키로 인증 확인
-        if (location.pathname === "/login" || location.pathname === "/set-nickname") {
-          router.replace("/"); // 이미 로그인 중이면 메인으로
-        }
-      } catch (err) {
-        router.replace("/login"); // 로그인 안 되어 있으면 로그인 페이지로
-      } finally {
-        setChecked(true);
-      }
-    };
+    // 쿠키에서 인증 상태 확인
+    const token =
+      document.cookie.includes("ACCESS_TOKEN") || document.cookie.includes("REFRESH_TOKEN");
 
-    checkAuth();
+
+    if (!token) {
+      router.replace("/login");
+    }
+
   }, []);
-
-  if (!checked) return null;
 
   return <>{children}</>;
 };
+
 export default AuthGuard;
