3d16ef48bb78ebead008f3893a533b23fcc94345
Jang Eunhye <hh961130@naver.com>
2025-06-27 03:09:50 +0900

    fix: 로그아웃 오류 콘솔확인

--- src/apis/mypage/fetchLogout.ts
+++ src/apis/mypage/fetchLogout.ts
@@ -1,8 +1,27 @@
 import axiosInstance from "@/apis/utils/axiosInstance";
 
+// 쿠키에서 특정 값을 가져오는 헬퍼 함수
+// const getCookie = (name: string): string => {
+//   if (typeof document === "undefined") return "";
+//   return (
+//     document.cookie
+//       .split("; ")
+//       .find((row) => row.startsWith(name + "="))
+//       ?.split("=")[1] || ""
+//   );
+// };
+
 // 로그아웃 API
 const fetchLogout = async (): Promise<boolean> => {
-  const response = await axiosInstance.post("/users/auth/logout"); 
+  //const kakaoAccess = getCookie("KAKAO_ACCESS");
+
+  const response = await axiosInstance.post("/users/auth/logout", undefined, {
+    // headers: {
+    //   "Kakao-Access": kakaoAccess || "",
+    // },
+  });
+  console.log("response", response);
+  // console.log("kakaoAccess", kakaoAccess);
   return response.status === 204;
 };
 
