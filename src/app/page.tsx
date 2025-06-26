2d007d094e810ae4b6b7fdd1de2991d0e62a5a8c
hgTime <hgtime@gmail.com>
2025-06-27 02:40:30 +0900

    fix: 홈 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,10 @@
 "use client";
 
 import ChatPageLayout from "./chat/ChatPageLayout";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import { getUserInfo } from "@/apis/login/getUserInfo";
-import axiosInstance from "@/apis/utils/axiosInstance";
 import useAuthGuard from "@/hooks/common/useAuthGuard";
 
 export default function HomePage() {
-  const router = useRouter();
-  const [isReady, setIsReady] = useState(false);
-
   useAuthGuard();
-
-  if (!isReady) return null;
 
   return <ChatPageLayout currentChatId={null} />;
 }
