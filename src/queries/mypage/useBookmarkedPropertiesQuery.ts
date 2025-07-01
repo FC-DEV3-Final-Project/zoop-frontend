33daa0d4a62482e8198808d9e7e560c96660d02d
Jang Eunhye <hh961130@naver.com>
2025-07-01 14:40:43 +0900

    refactor: api 호출 query파일에 병합

--- src/queries/mypage/useBookmarkedPropertiesQuery.ts
+++ src/queries/mypage/useBookmarkedPropertiesQuery.ts
@@ -1,6 +1,23 @@
 import useInfiniteScroll from "@/hooks/common/useInfiniteScroll";
-import fetchBookmarkedProperties from "@/apis/mypage/fetchBookmarkedProperties";
 import { PropertyCardProps } from "@/components/common/PropertyCard";
+import axiosInstance from "@/apis/utils/axiosInstance";
+
+type BookmarkedPropertiesResponse = {
+  myProperties: PropertyCardProps[];
+  hasNext: boolean;
+};
+
+const fetchBookmarkedProperties = async (
+  page: number,
+  size: number = 2,
+  hasNext: boolean = true,
+): Promise<BookmarkedPropertiesResponse> => {
+    // 무한스크롤 테스트용 2초 대기
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    const response = await axiosInstance.get(`/mypage/histories/bookmarked-properties?page=${page}&size=${size}`);
+    return response.data.data;
+};
+
 
 export const useBookmarkedPropertiesQuery = (size: number = 2, enabled: boolean = true) => {
   const { items, loader, hasMore, loading, error, reset } = useInfiniteScroll<PropertyCardProps>(
