0a1a6a110e36e7b164f1e463f0e07bff5c58227f
hgTime <hgtime@gmail.com>
2025-07-02 11:30:14 +0900

    refactor: 닉네임 입력부분 query로 수정

--- src/apis/login/createUserNickname.ts
+++ src/apis/login/createUserNickname.ts
@@ -1,12 +1,28 @@
 import { UserNickname } from "@/types/user";
 import axiosInstance from "../utils/axiosInstance";
+import { useMutation } from "@tanstack/react-query";
+import { getUserInfo } from "./getUserInfo";
+import { useRouter } from "next/navigation";
+import { useUserInfoStore } from "@/stores/useUserInfoStore";
 
 export const createUserNickname = async (nickname: string): Promise<UserNickname> => {
-  try {
-    const response = await axiosInstance.post("/users/auth/register", { nickname });
-    return response.data;
-  } catch (error) {
-    console.error("createUserNickname 에러:", error);
-    throw error;
-  }
+  const response = await axiosInstance.post("/users/auth/register", { nickname });
+  return response.data;
+};
+
+export const useCreateNicknameMutation = () => {
+  const router = useRouter();
+  const { setUser } = useUserInfoStore();
+
+  return useMutation({
+    mutationFn: createUserNickname,
+    onSuccess: async () => {
+      const userData = await getUserInfo();
+      setUser(userData);
+      router.push("/");
+    },
+    onError: (error) => {
+      console.error("닉네임 등록 오류:", error);
+    },
+  });
 };
